ef82041cb905cff2fb172f2e0159b8d182619e93
Samarth Shrivastava <shrivatsavasamarth22@gmail.com>
2023-10-06 20:53:42 +0530

    Successfully created convexHull algorithm

--- models/Point.js
+++ models/Point.js
@@ -21,37 +21,3 @@ export class Point {
         this.longitude = value;
     }
 }
-
-export const initialRegion = {
-    latitude: 23.188670,
-    longitude: 77.446871,
-    latitudeDelta: 0.01,
-    longitudeDelta: 0.01
-}
-
-const MARKER1 = {
-    latitude: 23.188862,
-    longitude: 77.447398,
-    latitudeDelta: 0.02,
-    longitudeDelta: 0.02
-}
-
-const MARKER2 = {
-    latitude: 23.188912,
-    longitude: 77.447607,
-    latitudeDelta: 0.02,
-    longitudeDelta: 0.02
-}
-
-const MARKER3 = {
-    latitude: 23.189035,
-    longitude: 77.447899,
-    latitudeDelta: 0.02,
-    longitudeDelta: 0.02
-}
-
-const POINT1 = new Point(MARKER1, 'marker1')
-const POINT2 = new Point(MARKER2, 'marker2')
-const POINT3 = new Point(MARKER3, 'marker3')
-
-export const POINTS = [POINT1, POINT2, POINT3]
