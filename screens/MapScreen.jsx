219fc8bd4d0b016b7278216837dc56d26ff2cfe1
Samarth Shrivastava <shrivatsavasamarth22@gmail.com>
2023-10-09 19:00:36 +0530

    Implemented useLocation logic directly into MapScreen

--- screens/MapScreen.jsx
+++ screens/MapScreen.jsx
@@ -1,3 +1,8 @@
+import {
+    Accuracy,
+    requestBackgroundPermissionsAsync,
+    watchPositionAsync,
+} from "expo-location";
 import { ActivityIndicator, Button, StyleSheet, Text, View } from "react-native";
 import MapView, {
     Circle,
@@ -6,14 +11,13 @@ import MapView, {
     Polygon,
     Polyline,
 } from "react-native-maps";
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { Point } from "../models/Point";
 import { calculateConvexHull } from "../sortPoints";
+import customMapStyles from "../customMapStyles";
 import { haversine } from "haversine-distance";
 import { useIsFocused } from "@react-navigation/native";
-import useLocation  from "../hooks/useLocation";
 
 const MapScreen = () => {
     const initialRegion = {
@@ -40,199 +44,50 @@ const MapScreen = () => {
         }
     };
 
-    const callback = useCallback(
-        (location) => callBackFunction(location),
-        [recording]
-    );
+    const startLocationTracking = async () => {
+        let { status } = await requestBackgroundPermissionsAsync();
+        if (status !== "granted") {
+            setErrorMsg("Permission to access location was denied");
+            return;
+        }
 
-    const [err] = useLocation(isFocused || recording, callback);
+        const subscriber = await watchPositionAsync(
+            {
+                accuracy: Accuracy.BestForNavigation,
+                timeInterval: 500,
+                distanceInterval: 5,
+            },
+            callBackFunction
+        );
 
-    const customMapStyle = [
-        {
-            elementType: "geometry",
-            stylers: [
-                {
-                    color: "#212121",
-                },
-            ],
-        },
-        {
-            elementType: "labels.icon",
-            stylers: [
-                {
-                    visibility: "off",
-                },
-            ],
-        },
-        {
-            elementType: "labels.text.fill",
-            stylers: [
-                {
-                    color: "#757575",
-                },
-            ],
-        },
-        {
-            elementType: "labels.text.stroke",
-            stylers: [
-                {
-                    color: "#212121",
-                },
-            ],
-        },
-        {
-            featureType: "administrative",
-            elementType: "geometry",
-            stylers: [
-                {
-                    color: "#757575",
-                },
-            ],
-        },
-        {
-            featureType: "administrative.country",
-            elementType: "labels.text.fill",
-            stylers: [
-                {
-                    color: "#9e9e9e",
-                },
-            ],
-        },
-        {
-            featureType: "administrative.land_parcel",
-            stylers: [
-                {
-                    visibility: "off",
-                },
-            ],
-        },
-        {
-            featureType: "administrative.locality",
-            elementType: "labels.text.fill",
-            stylers: [
-                {
-                    color: "#bdbdbd",
-                },
-            ],
-        },
-        {
-            featureType: "poi",
-            elementType: "labels.text.fill",
-            stylers: [
-                {
-                    color: "#757575",
-                },
-            ],
-        },
-        {
-            featureType: "poi.park",
-            elementType: "geometry",
-            stylers: [
-                {
-                    color: "#181818",
-                },
-            ],
-        },
-        {
-            featureType: "poi.park",
-            elementType: "labels.text.fill",
-            stylers: [
-                {
-                    color: "#616161",
-                },
-            ],
-        },
-        {
-            featureType: "poi.park",
-            elementType: "labels.text.stroke",
-            stylers: [
-                {
-                    color: "#1b1b1b",
-                },
-            ],
-        },
-        {
-            featureType: "road",
-            elementType: "geometry.fill",
-            stylers: [
-                {
-                    color: "#2c2c2c",
-                },
-            ],
-        },
-        {
-            featureType: "road",
-            elementType: "labels.text.fill",
-            stylers: [
-                {
-                    color: "#8a8a8a",
-                },
-            ],
-        },
-        {
-            featureType: "road.arterial",
-            elementType: "geometry",
-            stylers: [
-                {
-                    color: "#373737",
-                },
-            ],
-        },
-        {
-            featureType: "road.highway",
-            elementType: "geometry",
-            stylers: [
-                {
-                    color: "#3c3c3c",
-                },
-            ],
-        },
-        {
-            featureType: "road.highway.controlled_access",
-            elementType: "geometry",
-            stylers: [
-                {
-                    color: "#4e4e4e",
-                },
-            ],
-        },
-        {
-            featureType: "road.local",
-            elementType: "labels.text.fill",
-            stylers: [
-                {
-                    color: "#616161",
-                },
-            ],
-        },
-        {
-            featureType: "transit",
-            elementType: "labels.text.fill",
-            stylers: [
-                {
-                    color: "#757575",
-                },
-            ],
-        },
-        {
-            featureType: "water",
-            elementType: "geometry",
-            stylers: [
-                {
-                    color: "#000000",
-                },
-            ],
-        },
-        {
-            featureType: "water",
-            elementType: "labels.text.fill",
-            stylers: [
-                {
-                    color: "#3d3d3d",
-                },
-            ],
-        },
-    ];
+        return subscriber;
+    };
+
+    const stopLocationTracking = (subscriber) => {
+        if (subscriber) {
+            subscriber.remove();
+        }
+    };
+
+    
+    const [subscriber, setSubscriber] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+    
+    useEffect(() => {
+        if (isFocused || recording) {
+            startLocationTracking().then((newSubscriber) => {
+                setSubscriber(newSubscriber);
+            });
+        } else {
+            stopLocationTracking(subscriber);
+        }
+
+        return () => {
+            stopLocationTracking(subscriber);
+        };
+    }, [isFocused, recording]);
+
+    
 
     if (currentLocation) {
         console.log(currentLocation.coords);
@@ -260,7 +115,7 @@ const MapScreen = () => {
                     provider={PROVIDER_GOOGLE}
                     initialRegion={initRegion}
                     style={styles.mapStyle}
-                    customMapStyle={customMapStyle}
+                    customMapStyle={customMapStyles}
                 >
                     <Circle
                         center={{
@@ -314,7 +169,11 @@ const MapScreen = () => {
                 justifyContent: "center",
             }}
         >
-            <ActivityIndicator size={48} color="#56B532" />
+            {errorMsg ? (
+                <Text>{errorMsg}</Text>
+            ) : (
+                <ActivityIndicator size={48} color="#56B532" />
+            )}
         </View>
     );
 };
