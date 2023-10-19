1150bcddb1c479ae42c0f2db03b7dbb6bfddfe50
Samarth Shrivastava <shrivatsavasamarth22@gmail.com>
2023-10-19 19:41:08 +0530

    Added MapView

--- screens/MapScreen.jsx
+++ screens/MapScreen.jsx
@@ -1,9 +1,11 @@
 import * as Location from "expo-location";
 import * as TaskManager from "expo-task-manager";
 
-import  MapView, { Marker } from "react-native-maps";
+import  MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native"
+
+import customMapStyles  from '../customMapStyles'
 
 const MapScreen = ({ navigation }) => {
     const [location, setLocation] = useState(null);
@@ -26,5 +28,47 @@ const MapScreen = ({ navigation }) => {
 
         })();
     }, [])
+
+    if (location) {
+        <View style={styles.container}>
+            <MapView
+                provider={PROVIDER_GOOGLE}
+                initialCamera={location}
+                style={styles.mapStyle}
+                customMapStyle={customMapStyles}
+            >
+                <Marker
+                    coordinate={location}
+                    title="My Location"
+                    description="I am here"
+                />
+            </MapView>
+        </View>
+    } else {
+        return (
+            <View style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#fff"
+            }}>
+                <Text>
+                    {errorMsg}
+                </Text>
+            </View>
+        )
+    }
 }
 
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        resizeMode: "cover",
+        backgroundColor: "#fff",
+    },
+    mapStyle: {
+        width: "100%",
+        height: "100%",
+    }
+})
